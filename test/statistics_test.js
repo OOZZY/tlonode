7d8cdb11879f347cebf51d390faca7f199477021
oozzy <oozzy.tlo@gmail.com>
2017-07-01 10:14:52 -0700

    test: Use chai.assert's closeTo() and isNull()

--- test/statistics_test.js
+++ test/statistics_test.js
@@ -1,20 +1,22 @@
 "use strict";
 
-const assert = require('assert');
+const assert = require('chai').assert;
 const tlostatistics = require('tlo/statistics');
+
+const EPSILON = Number.EPSILON;
 
 describe('tlo/statistics', function() {
   describe('Statistics constructor', function() {
     it('should initialize statistics', function () {
       var statistics = new tlostatistics.Statistics;
-      assert.equal(statistics.size, 0);
-      assert.equal(statistics.sum, null);
-      assert.equal(statistics.mean, null);
-      assert.equal(statistics.min, null);
-      assert.equal(statistics.max, null);
-      assert.equal(statistics.range, null);
-      assert.equal(statistics.variance, null);
-      assert.equal(statistics.stddeviation, null);
+      assert.closeTo(statistics.size, 0, EPSILON);
+      assert.isNull(statistics.sum);
+      assert.isNull(statistics.mean);
+      assert.isNull(statistics.min);
+      assert.isNull(statistics.max);
+      assert.isNull(statistics.range);
+      assert.isNull(statistics.variance);
+      assert.isNull(statistics.stddeviation);
     });
   });
 
@@ -22,28 +24,28 @@ describe('tlo/statistics', function() {
     it('should update statistics over one call', function () {
       var statistics = new tlostatistics.Statistics;
       statistics.add(50.0);
-      assert.equal(statistics.size, 1);
-      assert.equal(statistics.sum, 50);
-      assert.equal(statistics.mean, 50);
-      assert.equal(statistics.min, 50);
-      assert.equal(statistics.max, 50);
-      assert.equal(statistics.range, 0);
-      assert.equal(statistics.variance, 0);
-      assert.equal(statistics.stddeviation, 0);
+      assert.closeTo(statistics.size, 1, EPSILON);
+      assert.closeTo(statistics.sum, 50, EPSILON);
+      assert.closeTo(statistics.mean, 50, EPSILON);
+      assert.closeTo(statistics.min, 50, EPSILON);
+      assert.closeTo(statistics.max, 50, EPSILON);
+      assert.closeTo(statistics.range, 0, EPSILON);
+      assert.closeTo(statistics.variance, 0, EPSILON);
+      assert.closeTo(statistics.stddeviation, 0, EPSILON);
     });
 
     it('should update statistics over multiple calls', function () {
       var statistics = new tlostatistics.Statistics;
       statistics.add(50.0);
       statistics.add(100.0);
-      assert.equal(statistics.size, 2);
-      assert.equal(statistics.sum, 150);
-      assert.equal(statistics.mean, 75);
-      assert.equal(statistics.min, 50);
-      assert.equal(statistics.max, 100);
-      assert.equal(statistics.range, 50);
-      assert.equal(statistics.variance, 625);
-      assert.equal(statistics.stddeviation, 25);
+      assert.closeTo(statistics.size, 2, EPSILON);
+      assert.closeTo(statistics.sum, 150, EPSILON);
+      assert.closeTo(statistics.mean, 75, EPSILON);
+      assert.closeTo(statistics.min, 50, EPSILON);
+      assert.closeTo(statistics.max, 100, EPSILON);
+      assert.closeTo(statistics.range, 50, EPSILON);
+      assert.closeTo(statistics.variance, 625, EPSILON);
+      assert.closeTo(statistics.stddeviation, 25, EPSILON);
     });
   });
 });
